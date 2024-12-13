e3dd93457ec4903b4a994f49476ac6f9c26bf88a
Jec-Masuda <masuda@jec.ac.jp>
2024-12-14 00:20:54 +0900

    接続文字列変更

--- helpers/config.php
+++ helpers/config.php
@@ -1,3 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server = tcp:jn9903db.database.windows.net,1433; Database = jn9903", "jndb", "Pa$$word1234');
+    define('DSN', 'sqlsrv:server = tcp:jn9903db.database.windows.net,1433; Database = jn9903');
+    define('DB_USER', 'jndb');
+    define('DB_PASSWORD', 'Pa$$word1234');
