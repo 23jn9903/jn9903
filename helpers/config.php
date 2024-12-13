080f3976c55890aad6dbfe0950c924f4ef21c5aa
Jec-Masuda <masuda@jec.ac.jp>
2024-12-14 00:12:31 +0900

    DBパスワード修正

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server = tcp:jn9903db.database.windows.net,1433; Database = jn9903", "jndb", "{Pa$$word1234}');
+    define('DSN', 'sqlsrv:server = tcp:jn9903db.database.windows.net,1433; Database = jn9903", "jndb", "Pa$$word1234');
     define('DB_USER', '20jn0300');
     define('DB_PASSWORD', '20jn0300');
